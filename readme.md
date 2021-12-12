8e98207c20b44f666561ec93c71285756a73251b
rullyafrizal <rullyafrizal4@gmail.com>
2021-12-12 19:39:24 +0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Description
+- This project is intended to be used for practice in particular subject such as algorithm and data structures using Typescript
+
+## Prerequisites
+- NodeJS
+- Typescript
+
+## Installation
+- `npm install` or `yarn install`
+- for watch mode : `npm run watch` or `yarn run watch` or `tsc -w`
+- for build mode : `npm run build` or `yarn run build` or `tsc`
+- `node dist/{file output you want to run}`
---
+++ src/invertBinaryTree.ts
@@ -0,0 +1,55 @@
+interface TreeNode {
+  val: number,
+  left: TreeNode | any,
+  right: TreeNode | any
+}
+
+const root: TreeNode = {
+  val: 4,
+  left: {
+    val: 2,
+    left: {
+      val: 1
+    },
+    right: {
+      val: 3
+    }
+  },
+  right: {
+    val: 7,
+    left: {
+      val: 6
+    },
+    right: {
+      val: 9
+    }
+  }
+}
+
+const invertTree = (root: TreeNode | null): TreeNode | null => {
+  if (isTreeNode(root)) {
+    const left = root.left
+    root.left = root.right
+    root.right = left
+
+    invertTree(root.left)
+    invertTree(root.right)
+  }
+  return root
+}
+
+const isTreeNode = (root: any): root is TreeNode => {
+  return root != null
+    && typeof root === 'object'
+    && typeof root.val === 'number'
+    && typeof root.left === 'object'
+    && typeof root.right === 'object'
+}
+
+console.log(root)
+
+invertTree(root)
+
+console.log(root)
+
+
